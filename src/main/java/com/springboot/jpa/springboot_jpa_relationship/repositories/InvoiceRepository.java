19a9b15fa9555a8c6242ee13d9e85166de4e12ce
Santiago Varela <santiago.vrl.12@gmail.com>
2024-07-26 18:31:37 -0300

    Creacion del proyecto y clases

---
+++ src/main/java/com/springboot/jpa/springboot_jpa_relationship/repositories/InvoiceRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.jpa.springboot_jpa_relationship.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.springboot.jpa.springboot_jpa_relationship.entities.Invoice;
+
+public interface InvoiceRepository extends CrudRepository<Invoice, Long>{
+
+    
+}
